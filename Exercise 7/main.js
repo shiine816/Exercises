aa52a3986944dd4c38b074464f439dab21a411a1
jack <abubakarcadde1000@gmail.com>
2026-04-20 14:06:39 +0300

    added index file

---
+++ Exercise 7/main.js
@@ -0,0 +1,21 @@
+let colors = ["white" ,  "blue" , "black" ]
+console.log("after push") ;
+colors.push("mohan");
+console.log(colors.length);
+console.log(colors)
+
+console.log("after shift") ;
+colors.shift();
+console.log(colors)
+
+console.log("after unshift") ;
+colors.unshift("hassan");
+console.log(colors)
+
+console.log("after pop") ;
+colors.pop();
+console.log(colors)
+
+console.log("after length") ;
+console.log(colors.length);
+console.log(colors)
