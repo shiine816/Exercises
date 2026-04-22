84c7f3b050751222536c204cfa82074a458ce5bd
jack <abubakarcadde1000@gmail.com>
2026-04-22 06:16:43 +0300

    added index file

---
+++ Exercise 9/main.js
@@ -0,0 +1,14 @@
+let car={
+Make: "japan",
+Model: "Gt03",
+year:2004, 
+start :function(){
+    console.log("the car has started");
+}
+
+};
+
+console.log(car.Make);
+console.log(car.Model);
+console.log(car.year);
+car.start();
