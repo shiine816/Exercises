c96b075957142e9cb5c0a39ea9e64ebc46b4a5c8
jack <abubakarcadde1000@gmail.com>
2026-05-02 14:40:49 +0300

    added file index

---
+++ Ecercise 10/script.js
@@ -0,0 +1,8 @@
+let library = [
+    { title: "Chapter 5", author: "s dvjg", year: 2010 },
+    { title: "Chapter 6", author: " vgxfx", year: 2010 },
+    { title: "Chapter 7", author: "s dvjg", year: 2020 },
+];
+
+console.log(library[0].title);
+console.log(library[1].author);
