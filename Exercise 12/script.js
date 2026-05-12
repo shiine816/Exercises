99fab3fab6bf9d86f4a7a413182558e168a5db63
jack <abubakarcadde1000@gmail.com>
2026-05-12 12:03:47 +0300

    added index file

---
+++ Exercise 12/script.js
@@ -0,0 +1,12 @@
+
+
+
+
+const ClassStudent = ["Ahmed", "nour", "lie", "mohamud", "mohamed", "sami", "yousef", "omar", "hassan", "abdulaziz"];
+
+console.log(ClassStudent.length);
+
+for (let i = 0; i < ClassStudent.length; i++) {
+  console.log(ClassStudent[i]);
+}
+
